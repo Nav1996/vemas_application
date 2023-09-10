32d360daeaacb42bb32536cada235802d0db2b72
Nav1996 <2017cs122@stu.ucsc.cmb.ac.lk>
2023-09-10 22:35:54 +0530

    started screen

--- app/src/main/java/com/example/vemasapplication/startedscreen.java
+++ app/src/main/java/com/example/vemasapplication/startedscreen.java
@@ -3,13 +3,27 @@ package com.example.vemasapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class startedscreen extends Activity {
+
+    private static final long SPLASH_DISPLAY_DURATION = 2000; // 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startedscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start your main activity after the splash screen duration
+                Intent mainIntent = new Intent(startedscreen.this, login.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_DURATION);
     }
 }
